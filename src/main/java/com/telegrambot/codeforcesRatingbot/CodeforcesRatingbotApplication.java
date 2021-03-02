6cb565da3a9e33f016e3b79d76e80bd5c644e775
Aibyn Khaireden <aibyn.h.1@gmail.com>
2021-03-02 12:01:20 +0500

    Added Gradle project

---
+++ src/main/java/com/telegrambot/codeforcesRatingbot/CodeforcesRatingbotApplication.java
@@ -0,0 +1,13 @@
+package com.telegrambot.codeforcesRatingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeforcesRatingbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeforcesRatingbotApplication.class, args);
+	}
+
+}
