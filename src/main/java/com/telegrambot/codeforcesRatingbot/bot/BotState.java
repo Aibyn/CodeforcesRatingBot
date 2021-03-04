004fae57577c549427f7117ef8e75d96ad9a6b38
Aibyn Khaireden <aibyn.h.1@gmail.com>
2021-03-04 14:43:58 +0500

    Added reply to commands.

---
+++ src/main/java/com/telegrambot/codeforcesRatingbot/bot/BotState.java
@@ -0,0 +1,12 @@
+package com.telegrambot.codeforcesRatingbot.bot;
+
+public enum BotState {
+    SHOW_HELP_MENU,
+    SHOW_MAIN_MENU,
+    SUBSCRIPTION_START,
+    SUBSCRIPTION_PROCESS,
+    UNSUBSCRIPTION_START,
+    UNSUBSCRIPTION_PROCESS,
+    SHOW_SUBSCRIPTION_LIST,
+    NULL_STATE;
+}
