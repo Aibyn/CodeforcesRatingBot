56b30f5700d364e45eb7be69960ab0f53275f863
Aibyn Khaireden <aibyn.h.1@gmail.com>
2021-03-05 19:16:01 +0500

    Added InfoService to retrieve user rating from api.

--- src/main/java/com/telegrambot/codeforcesRatingbot/CodeforcesRatingBotApplication.java
+++ src/main/java/com/telegrambot/codeforcesRatingbot/CodeforcesRatingBotApplication.java
@@ -1,7 +1,9 @@
 package com.telegrambot.codeforcesRatingbot;
 
+import com.telegrambot.codeforcesRatingbot.service.InfoRetrievingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +15,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
-public class CodeforcesRatingBotApplication {
+public class CodeforcesRatingBotApplication implements CommandLineRunner {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	InfoRetrievingService infoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeforcesRatingBotApplication.class, args);
 	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Here start get codeforces");
+		infoService.retrieveJson("Shadow");
+	}
 }
