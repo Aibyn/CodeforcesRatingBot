b0e092d62591f2f78af4e034e981aeb2639792ce
Aibyn Khaireden <aibyn.h.1@gmail.com>
2021-03-11 15:57:50 +0500

    Rewritten profile list to look better.

--- src/main/java/com/telegrambot/codeforcesRatingbot/service/InfoRetrievingService.java
+++ src/main/java/com/telegrambot/codeforcesRatingbot/service/InfoRetrievingService.java
@@ -3,6 +3,7 @@ package com.telegrambot.codeforcesRatingbot.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.telegrambot.codeforcesRatingbot.model.Profile;
 import com.telegrambot.codeforcesRatingbot.model.RatingChange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +15,14 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class InfoRetrievingService {
 
     String codeforcesApiUrl = "https://codeforces.com/api/";
     String codeforcesGetUserRating = "user.rating";
+    String codeforcesGetUser = "user.info";
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -68,5 +71,39 @@ public class InfoRetrievingService {
             throw new RuntimeException("Couldn't get username too many requests");
         }
         return ratingChange;
+    }
+    public Profile retrieveProfileByUsername(String username) throws RuntimeException {
+        String url = codeforcesApiUrl + codeforcesGetUser + "?handles=" + username;
+        int tryCount = 5;
+        boolean again = true;
+        Profile profile = null;
+        while (again && tryCount > 0) {
+            tryCount--;
+            try {
+                String jsonString = restTemplate.getForObject(url, String.class);
+                try {
+                    JsonNode jsonNode = objectMapper.readTree(jsonString).findPath("result");
+                    Profile[] profileList = objectMapper.readValue(jsonNode.toString(), Profile[].class);
+                    profile = profileList[0];
+                    again = false;
+                } catch (JsonProcessingException e) {
+                    again = false;
+                    e.printStackTrace();
+                    throw new RuntimeException("Couldn't parse the Json");
+                }
+            } catch (HttpClientErrorException.TooManyRequests e) {
+                logger.warn("Too many requests to Codeforces API");
+            } catch (HttpClientErrorException.BadRequest e) {
+                logger.info("Non existent username={}", username);
+                throw e;
+            } catch (HttpClientErrorException e) {
+                logger.warn("Http 400 exceptions -> {} for this username -> {}", e.getLocalizedMessage(), username);
+                throw new RuntimeException("Couldn't access. Http 400 type error" );
+            }
+        }
+        if (again) {
+            throw new RuntimeException("Couldn't get username too many requests");
+        }
+        return profile;
     }
 }
