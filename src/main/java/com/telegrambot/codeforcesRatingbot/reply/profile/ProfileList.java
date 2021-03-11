917d9c6fab6bf22ea0a64a8efcfb76494eb434b9
Aibyn Khaireden <aibyn.h.1@gmail.com>
2021-03-11 15:57:50 +0500

    Rewritten profile list to look better.

--- src/main/java/com/telegrambot/codeforcesRatingbot/reply/profile/ProfileList.java
+++ src/main/java/com/telegrambot/codeforcesRatingbot/reply/profile/ProfileList.java
@@ -2,17 +2,22 @@ package com.telegrambot.codeforcesRatingbot.reply.profile;
 
 import com.telegrambot.codeforcesRatingbot.bot.BotState;
 import com.telegrambot.codeforcesRatingbot.cache.UserCache;
+import com.telegrambot.codeforcesRatingbot.model.Profile;
 import com.telegrambot.codeforcesRatingbot.model.UserRatingSubscription;
 import com.telegrambot.codeforcesRatingbot.reply.Reply;
 import com.telegrambot.codeforcesRatingbot.sender.CommonMessages;
+import com.telegrambot.codeforcesRatingbot.service.InfoRetrievingService;
 import com.telegrambot.codeforcesRatingbot.service.UserRatingRepositoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.util.Arrays;
+import java.util.Formatter;
 import java.util.List;
 
 @Service
@@ -21,6 +26,8 @@ public class ProfileList implements Reply {
 
     @Autowired
     UserRatingRepositoryService subscriptionService;
+    @Autowired
+    InfoRetrievingService retrievingService;
     @Autowired
     CommonMessages messageService;
     @Autowired
@@ -35,7 +42,15 @@ public class ProfileList implements Reply {
         }
         StringBuilder stringBuilder = new StringBuilder();
         profileList.forEach(userRatingSubscription -> {
-            stringBuilder.append("*" + userRatingSubscription.getProfile() + "*\n");
+            String username = userRatingSubscription.getProfile();
+            Profile profile = retrievingService.retrieveProfileByUsername(username);
+            StringBuilder rank = new StringBuilder();
+            Arrays.stream(profile.getRank().split(" ")).forEach(word -> rank.append(StringUtils.capitalize(word) + " "));
+            rank.deleteCharAt(rank.length() - 1);
+            String rating = "(" + Integer.toString(profile.getRating()) + ")";
+            String handle = "[" + profile.getHandle() + "]" + "(https://codeforces.com/profile/" + profile.getHandle()+")";
+            String line = String.format("%s %s %s", rank, rating, handle);
+            stringBuilder.append(line + "\n");
         });
         stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         return messageService.sendMessage(chatId, "reply.list.profiles", stringBuilder.toString());
