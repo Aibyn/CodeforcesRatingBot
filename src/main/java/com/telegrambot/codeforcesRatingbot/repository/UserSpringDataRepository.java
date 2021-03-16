d076c96dee01882ea5089924b9d83258d83b68e7
Aibyn Khaireden <aibyn.h.1@gmail.com>
2021-03-16 23:06:06 +0500

    Fixed database postgresql.

--- src/main/java/com/telegrambot/codeforcesRatingbot/repository/UserSpringDataRepository.java
+++ src/main/java/com/telegrambot/codeforcesRatingbot/repository/UserSpringDataRepository.java
@@ -2,6 +2,7 @@ package com.telegrambot.codeforcesRatingbot.repository;
 
 import com.telegrambot.codeforcesRatingbot.model.UserRatingSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
